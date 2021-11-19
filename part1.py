c0bdd0a90596d1af895db88be9a6b9879ee622ee
Eaindra Naing <79598728+eaindra-naing@users.noreply.github.com>
2021-11-19 19:43:26 +0000

    Started thinking about part 1

--- part1.py
+++ part1.py
@@ -5,10 +5,22 @@ For example, sumofsquares(3) should return 14, since 1 + 4 + 9 = 14.
 """
 
 def sumofsquares(number):
-  pass  # delete this line when you start defining the function
+  sums = []
+  while number > 0:
+    for i in range (0,number):
+      sums.append (i * i)
+    print(sum(sums))
 
 
 
+#not sure what i did pls fix
 
+
+
+"""
+if program == "1":
+  n = int(input("Enter a number: "))
+  print(part1.sumofsquares(n))
 
 #need to work on
+"""
